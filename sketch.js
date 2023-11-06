8431ef70918237101d3a23267c655ccce7545225
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-06 13:57:10 -0600

    Add button click event to create grid
    Also restricted user input to be an integer between 1-100 and defaults
    to 16

--- sketch.js
+++ sketch.js
@@ -3,8 +3,16 @@ const button = document.querySelector("button");
 const GRID_PIXEL_SIZE = 700;
 let gridSquares = 16;
 
+button.addEventListener("click", newGrid)
+
 function newGrid() {
-    gridSquares = prompt();
+    gridSquares = Math.floor(Number.parseFloat(prompt("How many squares? (1-100)", 16))) || 16;
+    gridSquares > 100 ? gridSquares = 100 : gridSquares = gridSquares;
+    gridSquares < 1 ? gridSquares = 1 : gridSquares = gridSquares;
+
+    for (let element of document.querySelectorAll(".grid-container > div")) {
+        element.remove();
+    }
 
     for (let i = 0; i < gridSquares; i++) {
         for (let j = 0; j < gridSquares; j++) {
