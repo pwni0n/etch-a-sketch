1d572ac752b124865db4eda6629d306bdd6a8455
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-06 13:42:12 -0600

    Add function to create grid

--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
 const gridContainer = document.querySelector(".grid-container");
+const button = document.querySelector("button");
 const GRID_PIXEL_SIZE = 700;
 let gridSquares = 16;
 
-for (let i = 0; i < gridSquares; i++) {
-    for (let j = 0; j < gridSquares; j++) {
-        const gridItem = document.createElement("div");
-        gridItem.style.height = `${GRID_PIXEL_SIZE / gridSquares}px`;
-        gridItem.style.width = `${GRID_PIXEL_SIZE / gridSquares}px`;
-        gridItem.style.border = "1px solid black"
-        gridContainer.appendChild(gridItem);
+function newGrid() {
+    gridSquares = prompt();
 
-        gridItem.addEventListener("mouseenter", e => {
-            e.target.style.backgroundColor = "black";
-        })
+    for (let i = 0; i < gridSquares; i++) {
+        for (let j = 0; j < gridSquares; j++) {
+            const gridItem = document.createElement("div");
+            gridItem.style.height = `${GRID_PIXEL_SIZE / gridSquares}px`;
+            gridItem.style.width = `${GRID_PIXEL_SIZE / gridSquares}px`;
+            gridContainer.appendChild(gridItem);
+    
+            gridItem.addEventListener("mouseenter", e => {
+                e.target.style.backgroundColor = "black";
+            })
+        }
     }
 }
+
+newGrid();
