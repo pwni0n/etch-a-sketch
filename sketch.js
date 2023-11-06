fd417c957c770b1fb6b57476588a60f6f4fec025
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-06 13:30:39 -0600

    Add customizable grid sizing

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const gridContainer = document.querySelector(".grid-container");
+const GRID_PIXEL_SIZE = 700;
+let gridSquares = 16;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+for (let i = 0; i < gridSquares; i++) {
+    for (let j = 0; j < gridSquares; j++) {
         const gridItem = document.createElement("div");
-        gridItem.style.height = "30px";
-        gridItem.style.width = "30px";
+        gridItem.style.height = `${GRID_PIXEL_SIZE / gridSquares}px`;
+        gridItem.style.width = `${GRID_PIXEL_SIZE / gridSquares}px`;
         gridItem.style.border = "1px solid black"
         gridContainer.appendChild(gridItem);
 
