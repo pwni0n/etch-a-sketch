f338c0f2586aeb08a957b4edabaf9447dbaf40a7
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-06 13:10:40 -0600

    Add loops to create grid elements

---
+++ sketch.js
@@ -0,0 +1,11 @@
+const gridContainer = document.querySelector(".grid-container");
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const gridItem = document.createElement("div");
+        gridItem.style.height = "30px";
+        gridItem.style.width = "30px";
+        gridItem.style.border = "1px solid black"
+        gridContainer.appendChild(gridItem);
+    }
+}
